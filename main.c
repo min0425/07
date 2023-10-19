2e2976eea2c84b2119dffa0cfc7b0a90d98c42e6
min0425 <2310570@sookmyung.ac.kr>
2023-10-19 16:03:58 +0900

    4(2)

--- main.c
+++ main.c
@@ -3,18 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int counter;
-
-void set_counter()
-{counter = 20;
-}
+int i;
 int main(void)
 {
-	printf("counter=%d\n",counter);
-	set_counter();
-	printf("counter=%d\n",counter);
+	for ( i=0; i<5; i++ )
+	{//같은 지역변수 i이기 때문에 f()에 의해 이미 i가 지금 10이 되어버려서 i<5라는 조건이 성립되지 않고 따라사 한번만 반복하고 끝남. 
+		f();
+	}
 	return 0;
 }
+void f(void)
+{
+	for ( i=0; i<10; i++)
+	printf("#");
+}
+
 
 
 
